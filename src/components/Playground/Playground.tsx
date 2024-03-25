c1545dd789ca8ef62fc377f6d6ff433b0cc82efb
Anton Eliseev <anton_saintp@mail.ru>
2024-03-25 17:58:53 +0300

    timer

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -1,16 +1,34 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { setCurrentStep } from "./store/slices"
 import Controls from "./components/Controls"
+import { INTERVAL_TIME } from "./constants"
 
 const Playground: React.FC = () => {
   const { currentStep } = useAppSelector((state) => state.playground)
   const dispatch = useAppDispatch()
 
+  const refreshIntervalId = useRef<ReturnType<typeof setInterval> | null>(null)
+
   const [isTimerActive, setIsTimerActive] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (isTimerActive) {
+      refreshIntervalId.current = setInterval(() => {
+        dispatch(setCurrentStep())
+      }, INTERVAL_TIME)
+    } else {
+      clearInterval(refreshIntervalId.current as NodeJS.Timeout)
+    }
+
+    return () => {
+      clearInterval(refreshIntervalId.current as NodeJS.Timeout)
+    }
+  }, [isTimerActive])
 
   return (
     <>
+      {currentStep}
       <Controls
         isTimerActive={isTimerActive}
         setIsTimerActive={setIsTimerActive}
